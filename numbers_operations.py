5b79bbf78f7184eb0f7d6487fcd791d9b9704802
FilosofDanil <serhio3347@gmail.com>
2023-05-02 22:14:38 +0200

    Added following changes: -Fixed Moda -Median implementation

--- numbers_operations.py
+++ numbers_operations.py
@@ -25,11 +25,25 @@ class NumbersOperations:
         index = 0
         value = 0
         freq_set = self.get_frequencies_in_set(set)
+        set = self.get_unique(set)
+        moda_list = []
         for i in range(len(freq_set)):
             if freq_set[i] > value:
                 value = freq_set[i]
                 index = i
-        return set[index]
+                moda_list.clear()
+            if freq_set[i]==value:
+                moda_list.append(set[i])
+        return moda_list
+
+    def get_median(self, set):
+        sorted_set = sorted(set)
+        for i in range((int)(len(sorted_set) / 2 - 1)):
+            sorted_set.pop(0)
+            sorted_set.pop(len(sorted_set)-1)
+        if len(sorted_set) == 2:
+            return (sorted_set[0]+sorted_set[1])/2
+        return sorted_set[1]
 
         average = self.get_selective_average(set)
         for x in set:
