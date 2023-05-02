da35688e7411d192e389b7bd45af383b0e691843
wilhelm wesser <wilhelmwesser228@gmail.com>
2023-05-02 23:18:55 +0300

    Fixed code moves

--- numbers_operations.py
+++ numbers_operations.py
@@ -20,21 +20,35 @@ class NumbersOperations:
     def get_selective_dispersion(self, set):
         n = len(set)
         selective_dispersion = 0
+        average = self.get_selective_average(set)
+        for x in set:
+            selective_dispersion += (x - average)**2
+        return selective_dispersion / (n - 1)
 
     def get_moda(self, set):
         index = 0
         value = 0
         freq_set = self.get_frequencies_in_set(set)
+        set = self.get_unique(set)
+        moda_list = []
         for i in range(len(freq_set)):
             if freq_set[i] > value:
                 value = freq_set[i]
                 index = i
-        return set[index]
+                moda_list.clear()
+            if freq_set[i]==value:
+                moda_list.append(set[i])
+        return moda_list
 
-        average = self.get_selective_average(set)
-        for x in set:
-            selective_dispersion += (x - average)**2
-        return selective_dispersion / (n - 1)
+    def get_median(self, set):
+        sorted_set = sorted(set)
+        for i in range((int)(len(sorted_set) / 2 - 1)):
+            sorted_set.pop(0)
+            sorted_set.pop(len(sorted_set)-1)
+        if len(sorted_set) == 2:
+            return (sorted_set[0]+sorted_set[1])/2
+        return sorted_set[1]
+
 
     def get_selective_standard_deviation(self, set):
         dispersion = self.get_selective_dispersion(set)
