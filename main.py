f34afe5c926b1e56bb241e33b4732c304efa50fe
wilhelm wesser <wilhelmwesser228@gmail.com>
2023-05-02 20:43:31 +0300

    Generating set, polygon and histogram

--- main.py
+++ main.py
@@ -4,3 +4,20 @@ import matplotlib.pyplot as plt
 # 32
 variant = 120 % 11 + 11 * 2
 print(variant)
+
+# GENERATING SET
+n = 134
+mu = 0
+sigma = 1.7
+
+set = np.random.normal(mu, sigma, n)
+_, counts = np.unique(set, return_counts=True)
+print(set, counts)
+
+# POLYGON
+plt.plot(set, counts)
+plt.show()
+
+# HISTOGRAM
+plt.hist(set)
+plt.show()
