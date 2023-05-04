03f67001240a2d15f5db75f760afad3e2bc361b0
Nazariy <nazar260804@gmail.com>
2023-05-05 02:10:32 +0300

    pareto+pie added, minor fixes and improvements

--- diagrams.py
+++ diagrams.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
 class Diagrams:
     def __init__(self):
         self.plt = plt
@@ -7,10 +9,31 @@ class Diagrams:
         self.plt.plot(x, y)
 
     def generate_histogram(self, x):
-        self.plt.hist(x)
+        n = len(np.unique(x))
+        self.plt.hist(x, bins=n, edgecolor='black')
 
     def generate_boxplot(self, x):
         self.plt.boxplot(x)
+
+    def generate_pie(self, x, y):
+        self.plt.rcParams['font.size'] = 8.0
+        self.plt.pie(x, labels=y)
+
+    def generate_pareto(self, x, y):
+        pairs = list(zip(x, y))
+        pairs.sort(key=lambda pair: pair[1], reverse=True)
+        x, y = zip(*pairs)
+        x = [str(i) for i in x]
+        self.plt.bar(x, y)
+        
+        cumsum = np.cumsum(y)
+        relative_cumsum = cumsum / cumsum[-1]
+
+        self.plt.twinx().plot(x, relative_cumsum, color='red', marker='o')
+        # put relative cumsum values on plot dots
+        for i in range(len(x)):
+            self.plt.text(x[i], relative_cumsum[i], str(round(relative_cumsum[i], 2)))
+
 
     def show_diagram(self):
         self.plt.show()
--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from diagrams import Diagrams
 diagrams = Diagrams()
 numbers_operations = NumbersOperations()
 
+# COLORS
+C_RED = '\033[91m'
+C_YELLOW = '\033[93m'
+C_END = '\033[0m'
+
 # 32
 variant = 120 % 11 + 11 * 2
-print(variant)
+print(f'{C_RED}Variant:{C_END} {variant}')
 
 # GENERATING SET
 n = 134
@@ -14,14 +19,21 @@ mu = 0
 sigma = 1.7
 
 set = numbers_operations.generate_set(mu, sigma, n)
-set = [round(x, 2) for x in set]
+set = [round(x, 0) for x in set]
 counts = numbers_operations.get_frequencies_in_set(set)
-set = numbers_operations.get_unique(set)
-print(set, counts)
+uniques = numbers_operations.get_unique(set)
+print(set)
 
+# STATISTICS
+
+print(f'{C_YELLOW}Середнє арифметичне:{C_END} {str(numbers_operations.get_selective_average(set))}')
+print(f"{C_YELLOW}Вибіркова дисперсія:{C_END} {numbers_operations.get_selective_dispersion(set)}")
+print(f"{C_YELLOW}Вибіркове стандартне відхилення:{C_END} {numbers_operations.get_selective_standard_deviation(set)}")
+print(f"{C_YELLOW}Медіана:{C_END} {str(numbers_operations.get_median(set))}" )
+print(f"{C_YELLOW}Мода:{C_END} {str(numbers_operations.get_moda(set)[0])}")
 
 # POLYGON
-diagrams.generate_polygon(set, counts)
+diagrams.generate_polygon(uniques, counts)
 diagrams.show_diagram()
 
 # HISTOGRAM
@@ -32,8 +44,12 @@ diagrams.show_diagram()
 diagrams.generate_boxplot(set)
 diagrams.show_diagram()
 
-print("Середнє арифметичне: " + str(numbers_operations.get_selective_average(set)))
-print(f"Вибіркова дисперсія: {numbers_operations.get_selective_dispersion(set)}")
-print(f"Вибіркове стандартне відхилення: {numbers_operations.get_selective_standard_deviation(set)}")
-print("Медіана: " + str(numbers_operations.get_median(set)))
-print("Мода: " + str(numbers_operations.get_moda(set)))
+# PIE PLOT
+diagrams.generate_pie(counts, uniques)
+diagrams.show_diagram()
+
+# PARETO PLOT
+diagrams.generate_pareto(uniques, counts)
+diagrams.show_diagram()
+
+
